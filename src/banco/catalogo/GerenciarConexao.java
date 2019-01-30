d772c013720e2ba019f19719cca983c4a90a9f13
Jonas Góes <jonasdossantos1991@gmail.com>
2019-01-30 17:44:38 -0200

    @Jonasdsg - backpoint

--- src/banco/catalogo/GerenciarConexao.java
+++ src/banco/catalogo/GerenciarConexao.java
@@ -1,5 +1,69 @@
 package banco.catalogo;
 
+import java.nio.channels.SeekableByteChannel;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class GerenciarConexao {
+    private static final String DRIVER ="com.mysql.jdbc.Driver";
+    private static final String URL ="jdbc:mysql//localhost:3306/catalogoloja";
+    private static final String USER ="root";
+    private static final String PASS ="";
     
+    public static Connection getConnection()
+    {
+        try {
+            Class.forName(DRIVER);
+            try {
+                return DriverManager.getConnection(URL, USER, PASS);
+            } catch (SQLException ex) {
+                Logger.getLogger(GerenciarConexao.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        } catch (ClassNotFoundException ex) {
+            throw new RuntimeException("Erro na conexão", ex);
+        }
+        return (null);
+    }
+    
+    public static void closeConnection(Connection conexao)
+    {
+        if(conexao != null)
+        {
+            try {
+                conexao.close();
+            } catch (SQLException ex) {
+                System.err.println("Erro ao finalizar conexao, relatorio ");
+            }
+        }
+    }
+    
+    public static void closeConnection(Connection conexao, PreparedStatement estado)
+    {
+        if(estado != null)
+        {
+            try {
+                estado.close();
+            } catch (SQLException ex) {
+                System.err.println("Erro ao finalizar conexao, relatorio ");
+            }
+        }
+    }
+    
+    public static void closeConnection(Connection conexao, PreparedStatement estado, ResultSet resultado)
+    {
+        if(resultado != null)
+        {
+            try {
+                resultado.close();
+            } catch (SQLException ex) {
+                System.err.println("Erro ao finalizar conexao, relatorio ");
+            }
+        }
+    }
+
 }
--- src/tela/login/FXMLTelaLoginController.java
+++ src/tela/login/FXMLTelaLoginController.java
@@ -11,6 +11,8 @@ import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.stage.Stage;
 
 
@@ -26,7 +28,8 @@ public class FXMLTelaLoginController implements Initializable {
     @FXML
     private Button botaoLogin;
     private Stage stage;
-
+    private KeyEvent monitor;
+    
     public void fazerLogin() throws Exception
     {
         if(nomeUsuario.getText().equals("jonas") && senhaUsuario.getText().equals("1234"))
@@ -43,7 +46,6 @@ public class FXMLTelaLoginController implements Initializable {
             alerta.setContentText("O usuário ou senha informados não conferem com o banco de dados.");
             alerta.show();
         }
-        
     }
     
     public void fecharTela()
