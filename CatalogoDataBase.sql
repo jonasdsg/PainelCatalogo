177e2b590a60f9b5a82d3783f686c36d66109803
Jonas Góes <jonasdossantos1991@gmail.com>
2019-01-30 15:23:46 -0200

    @Jonasdsg - Atualizações

--- CatalogoDataBase.sql
+++ CatalogoDataBase.sql
@@ -13,6 +13,7 @@ create table endereco(
 );
 create table Pessoa(
 	idPessoa		int not null primary key,
+    nomePessoa		varchar(255),
     telefone		varchar(255),
     celular			varchar(255),
     enderecofk		int not null,
@@ -24,6 +25,8 @@ create table PessoaFisica(
 	idPessoaFisica	int not null,
     cpf				varchar(255),
     rg				varchar(255),
+    enderecoCli		int not null,    
+    foreign key (enderecoCli) references endereco(cep),
     foreign key (idPessoaFisica) references Pessoa(idPessoa)
 );
 
@@ -43,3 +46,19 @@ create table Dispositivos(
     estadodoDispositivo		varchar(255),
     localArmazenado		varchar(255)
 );
+
+insert into endereco values
+(20040007,"Avenida Rio Branco",185,"Centro","Rio de Janeiro","Rio de Janeiro","Rio de Janeiro","RJ"),
+(26160265,"Estrada do Babi",77,"Vila Sao Sebastiao","Belford Roxo","Belford Roxo","Rio de Janeiro","RJ");
+
+insert into Pessoa values
+(1,"Jonas Goes","(021) 2262-5656","(021) 92970-7950",20040007);
+
+insert into PessoaFisica values 
+(1,"511.544.487-07","53.457.288-9",20040007);
+
+/* inner join Endereco on Pessoa.enderecofk=Endereco.cep; */
+select * from PessoaFisica;
+select Pessoa.nomePessoa as Nome, PessoaFisica.cpf as Identidade, endereco.rua as Endereco, endereco.numero as numero, endereco.cep as Cep from PessoaFisica 
+inner join Pessoa on Pessoa.idPessoa=PessoaFisica.idPessoaFisica
+inner join endereco on PessoaFisica.enderecoCli=endereco.cep;
